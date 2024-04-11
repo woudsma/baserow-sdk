d1deba77ac2b24eea2d994942366e4fef4ce5f3f
Nathan Arthur <nathan@nathanarthur.com>
2024-04-11 15:27:45 -0400

    add repo row class registration

--- src/codegen/makeRepositoryMethods.ts
+++ src/codegen/makeRepositoryMethods.ts
@@ -3,14 +3,16 @@ import { Table } from "../codegen.js";
 export default function makeRepositoryMethods(tables: Table[]): string {
   return tables
     .map(({ id, name }) => {
-      return `    public async getMany${name}(options: ListRowsOptions = {}): Promise<${name}Row[]> {
-        const {results} = await this.sdk.listRows<${name}RowType>(${id}, options);
-        return results.map((row) => new ${name}Row({tableId: ${id}, rowId: row.id, row, sdk: this.sdk, repository: this}));
+      return `    public async getMany${name}<T extends ${name}Row>(options: ListRowsOptions = {}): Promise<T[]> {
+        const { results } = await this.sdk.listRows<${name}RowType>(${id}, options);
+        const rowClass = this.getRowClass(${id}) || ${name}Row;
+        return results.map((row) => new rowClass({ tableId: ${id}, rowId: row.id, row, sdk: this.sdk, repository: this })) as T[];
     }
 
-    public async getOne${name}(id: number, options: GetRowOptions = {}): Promise<${name}Row> {
+    public async getOne${name}<T extends ${name}Row>(id: number, options: GetRowOptions = {}): Promise<T> {
         const row = await this.sdk.getRow<${name}RowType>(${id}, id, options);
-        return new ${name}Row({tableId: ${id}, rowId: row.id, row, sdk: this.sdk, repository: this});
+        const rowClass = this.getRowClass(${id}) || ${name}Row;
+        return new rowClass({ tableId: ${id}, rowId: row.id, row, sdk: this.sdk, repository: this }) as T;
     }
 `;
     })
--- src/factory.ts
+++ src/factory.ts
@@ -1,10 +1,26 @@
-import { getConfig } from "./getConfig.js";
+import { BaserowConfig, getConfig } from "./getConfig.js";
 import { BaserowSdk } from "./index.js";
+import { Row, RowOptions, RowType } from "./row.js";
+
+interface RowClass {
+  new (options: RowOptions<RowType>): Row<RowType>;
+}
 
 export abstract class Factory {
   protected sdk: BaserowSdk;
+  protected config: BaserowConfig;
+  protected classes: Map<number, RowClass> = new Map();
+
   constructor() {
-    const config = getConfig();
-    this.sdk = new BaserowSdk(config.databaseToken);
+    this.config = getConfig();
+    this.sdk = new BaserowSdk(this.config.databaseToken);
+  }
+
+  protected registerRowClass(tableId: number, rowClass: RowClass): void {
+    this.classes.set(tableId, rowClass);
+  }
+
+  protected getRowClass(tableId: number): RowClass | undefined {
+    return this.classes.get(tableId);
   }
 }
