f928d6e58b9efcda7d641b3ffb2fe2cdc3197b67
Nathan Arthur <nathan@nathanarthur.com>
2024-04-11 10:37:02 -0400

    don't require database token to instantiate factory

--- src/factory.ts
+++ src/factory.ts
@@ -1,8 +1,10 @@
+import { getConfig } from "./getConfig.js";
 import { BaserowSdk } from "./index.js";
 
 export abstract class Factory {
   protected sdk: BaserowSdk;
-  constructor({ sdk }: { sdk: BaserowSdk }) {
-    this.sdk = sdk;
+  constructor() {
+    const config = getConfig();
+    this.sdk = new BaserowSdk(config.databaseToken);
   }
 }
---
+++ src/getConfig.ts
@@ -0,0 +1,16 @@
+import rc from "rc";
+import z from "zod";
+
+const schema = z.object({
+  url: z.string(),
+  tables: z.record(z.string(), z.number()),
+  databaseToken: z.string(),
+  outDir: z.string(),
+  config: z.string(),
+});
+
+type BaserowConfig = z.infer<typeof schema>;
+
+export function getConfig(): BaserowConfig {
+  return schema.parse(rc("baserow"));
+}
