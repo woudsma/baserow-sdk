4b2c3c88ce056c75279715450f6654471532d549
Nathan Arthur <nathan@nathanarthur.com>
2024-04-12 10:39:55 -0400

    rework getConfig

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -1,18 +1,16 @@
 import rc from "rc";
 import z from "zod";
 
-const schema = z
-  .object({
-    url: z.string(),
-    tables: z.record(z.string(), z.number()),
-    databaseToken: z.string(),
-    outDir: z.string(),
-    config: z.string(),
-  })
-  .partial();
+const schema = z.object({
+  url: z.string(),
+  tables: z.record(z.string(), z.number()),
+  databaseToken: z.string(),
+  outDir: z.string(),
+  config: z.string(),
+});
 
 export type BaserowConfig = z.infer<typeof schema>;
 
 export function getConfig(): BaserowConfig {
-  return schema.parse(rc("baserow"));
+  return schema.parse(rc("baserow", { tables: {} }));
 }
