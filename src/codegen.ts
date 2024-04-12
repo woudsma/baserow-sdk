1b6f09492bb29c25f3ef6a0d36a46555a48c5cd9
Nathan Arthur <nathan@nathanarthur.com>
2024-04-12 10:04:47 -0400

    log config on error

--- src/codegen.ts
+++ src/codegen.ts
@@ -20,6 +20,7 @@ export default async function main(): Promise<void> {
     !config.tables
   ) {
     console.error("Missing required configuration options");
+    console.log(config);
     return;
   }
 
