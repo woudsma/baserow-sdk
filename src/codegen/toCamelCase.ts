332c118dce6edbd2cfb6556328d29258bb07c95f
Tjerk Woudsma <tjerkwoudsma@gmail.com>
2024-11-23 10:43:14 +0100

    Update toCamelCase.ts

--- src/codegen/toCamelCase.ts
+++ src/codegen/toCamelCase.ts
@@ -18,3 +18,7 @@ export function toCamelCase(str: string, upper: boolean = false): string {
 
   return _str;
 }
+
+function capitalizeFirstLetter(val: string) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
