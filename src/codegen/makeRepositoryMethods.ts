8eda81317d7442652942d3b503f3abf56af9c5bd
Tjerk Woudsma <tjerkwoudsma@gmail.com>
2024-11-23 10:47:30 +0100

    Update makeRepositoryMethods.ts

--- src/codegen/makeRepositoryMethods.ts
+++ src/codegen/makeRepositoryMethods.ts
@@ -1,20 +1,21 @@
 import { Table } from "../codegen.js";
-import { toCamelCase } from "./toCamelCase.js";
+import { toCamelCase, capitalizeFirstLetter } from "./toCamelCase.js";
 
 export default function makeRepositoryMethods(tables: Table[]): string {
   return tables
     .map(({ id, name }) => {
-      return `    public async getMany${name}<T extends ${name}Row>(options: ListRowsOptions = {}): Promise<T[]> {
+      const capitalizedName = capitalizeFirstLetter(name).split('_').map(capitalizeFirstLetter).join('');
+      return `    public async getMany${capitalizedName}<T extends ${name}Row>(options: ListRowsOptions = {}): Promise<T[]> {
       return this.getMany<T, ${name}RowType, Repository>(${id}, ${name}Row, options);
     }
 
-    public async getOne${name}<T extends ${name}Row>(id: number, options: GetRowOptions = {}): Promise<T> {
+    public async getOne${capitalizedName}<T extends ${name}Row>(id: number, options: GetRowOptions = {}): Promise<T> {
         const row = await this.sdk.getRow<${name}RowType>(${id}, id, options);
         const rowClass = this.getRowClass(${id}) || ${name}Row;
         return new rowClass({ tableId: ${id}, rowId: row.id, row, sdk: this.sdk, repository: this }) as T;
     }
 
-    public async addOne${toCamelCase(name)}(row: Omit<${name}RowType, "id" | "order">, options: AddRowOptions = {}): Promise<${name}RowType> {
+    public async addOne${capitalizedName}(row: Omit<${name}RowType, "id" | "order">, options: AddRowOptions = {}): Promise<${name}RowType> {
         return this.sdk.addRow<Omit<${name}RowType, "id" | "order">, ${name}RowType>(${id}, row, options);
     }
 `;
